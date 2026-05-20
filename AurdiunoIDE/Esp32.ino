7e31235da313c6f998fa1d7da657b438c6834dc1
Kurmana Sai sampath <135953334+Sampathkurmana@users.noreply.github.com>
2026-05-20 18:36:53 +0530

    Update WiFi credentials in Esp32.inoupdated wifi changes

--- AurdiunoIDE/Esp32.ino
+++ AurdiunoIDE/Esp32.ino
@@ -7,8 +7,8 @@
 // ==========================================
 
 // ----- YOUR MOBILE HOTSPOT CREDENTIALS -----
-const char* WIFI_SSID = "Sampath";           // Your hotspot name
-const char* WIFI_PASSWORD = "sampath577";    // Your hotspot password
+const char* WIFI_SSID = "Your wifi name";           // Your hotspot name
+const char* WIFI_PASSWORD = "your wifi passwords";    // Your hotspot password
 
 // ----- FIREBASE CONFIGURATION -----
 #define FIREBASE_URL "https://triangulation-4be54-default-rtdb.firebaseio.com"
